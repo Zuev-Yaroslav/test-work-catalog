a0c5b85f25340a8cae1234f0fb2fc14be9963779
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2026-01-22 13:58:42 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            tailwindcss(),
+            wayfinder({
+                formVariants: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        server: {
+            https: false,
+            host: true,
+            port: env.VITE_PORT,
+            hmr: {host: 'localhost', protocol: 'ws'},
+        },
+    }
+});
