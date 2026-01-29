f49756383bc4c1f6c1d45014d8331409f9c52f28
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2026-01-29 14:50:45 +0300

    Создал Resources для сущностей

---
+++ app/Http/Resources/ApiV1/User/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\ApiV1\User;
+
+use App\Http\Resources\ApiV1\Category\CategoryResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
