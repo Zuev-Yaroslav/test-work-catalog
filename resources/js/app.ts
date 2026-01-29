0125dc212f1c2293f931edf946b9be23e4cf993b
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2026-01-29 21:26:25 +0300

    в createApp добавил PrimeVue, ZiggyVue, Tooltip. Также переход на авторизацию неавторизованным пользователям.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,13 +1,26 @@
 import '../css/app.css';
+import 'vue-final-modal/style.css'
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import type { DefineComponent } from 'vue';
-import { createApp, h } from 'vue';
+import {createInertiaApp, router} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import type {DefineComponent} from 'vue';
+import {createApp, h} from 'vue';
+import {ZiggyVue} from 'ziggy-js';
+import PrimeVue from 'primevue/config';
+import Tooltip from 'primevue/tooltip';
+import Aura from '@primeuix/themes/aura';
 
-import { initializeTheme } from './composables/useAppearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+router.on('navigate', () => {
+    if (document.location.pathname.startsWith('/admin')) {
+        const token = localStorage.getItem('auth_token');
+        if (!token) {
+            router.visit(route('auth.login'));
+        }
+    }
+})
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
@@ -16,15 +29,20 @@ createInertiaApp({
             `./pages/${name}.vue`,
             import.meta.glob<DefineComponent>('./pages/**/*.vue'),
         ),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+
+        createApp({render: () => h(App, props)})
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
+            .use(ZiggyVue)
+            .directive('tooltip', Tooltip)
             .mount(el);
     },
     progress: {
         color: '#4B5563',
     },
 });
-
-// This will set light / dark mode on page load...
-initializeTheme();
